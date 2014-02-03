3505f15856d1e758034fa593bff6ab4d01801926
Daniel Tull <dt@danieltull.co.uk>
2014-02-03 08:31:16 +0000

    Use module style import

--- DCTDataSource Tests/DCTSplitDataSourceTests.m
+++ DCTDataSource Tests/DCTSplitDataSourceTests.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Daniel Tull. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+@import XCTest;
 #import "DCTTestDataSource.h"
 
 @interface DCTSplitDataSourceTests : XCTestCase
