c8b411b53a715c838d9ef8e67740d8f4e6b2837e
Daniel Tull <dt@danieltull.co.uk>
2014-02-24 15:39:31 +0000

    Improve example

--- DCTDataSource Demo/ViewController.m
+++ DCTDataSource Demo/ViewController.m
@@ -12,8 +12,10 @@
 
 @interface ViewController () <DCTCollectionViewDataSourceDelegate>
 @property (nonatomic) DCTCollectionViewDataSource *dataSource;
-@property (nonatomic) DCTSplitDataSource *split;
-@property (nonatomic) DCTArrayDataSource *list;
+@property (nonatomic) DCTSplitDataSource *split1;
+@property (nonatomic) DCTSplitDataSource *split2;
+@property (nonatomic) DCTArrayDataSource *list1;
+@property (nonatomic) DCTArrayDataSource *list2;
 @end
 
 @implementation ViewController
@@ -21,26 +23,45 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
-	DCTObjectDataSource *objectDS = [[DCTObjectDataSource alloc] initWithObject:@"zero"];
+	DCTObjectDataSource *objectDS1 = [[DCTObjectDataSource alloc] initWithObject:@"zero"];
 
-	self.list = [[DCTArrayDataSource alloc] initWithArray:@[ @"one", @"two", @"three", @"four", @"five", @"six", @"seven", @"eight", @"nine", @"ten" ]];
-	[self.list setUserInfoValue:@"cell2" forKey:DCTCollectionViewDataSourceUserInfoKeys.cellReuseIdentifier];
+	self.list1 = [[DCTArrayDataSource alloc] initWithArray:@[ @"one", @"two", @"three", @"four", @"five", @"six", @"seven", @"eight", @"nine", @"ten" ]];
+	[self.list1 setUserInfoValue:@"cell2" forKey:DCTCollectionViewDataSourceUserInfoKeys.cellReuseIdentifier];
 
-	self.split = [[DCTSplitDataSource alloc] initWithType:DCTSplitDataSourceTypeRow];
-	[self.split addChildDataSource:objectDS];
-	//[self.split addChildDataSource:self.list];
-	//[split addChildDataSource:objectDS3];
+	DCTObjectDataSource *objectDS2 = [[DCTObjectDataSource alloc] initWithObject:@"eleven"];
 
-	self.dataSource = [[DCTCollectionViewDataSource alloc] initWithCollectionView:self.collectionView dataSource:self.split];
+	self.list2 = [[DCTArrayDataSource alloc] initWithArray:@[ @"twelve", @"thirteen", @"fourteen", @"fifteen", @"sixteen", @"seventeen", @"eighteen", @"nineteen", @"twenty" ]];
+
+	[self.list2 setUserInfoValue:@"cell2" forKey:DCTCollectionViewDataSourceUserInfoKeys.cellReuseIdentifier];
+
+	self.split1 = [[DCTSplitDataSource alloc] initWithType:DCTSplitDataSourceTypeRow];
+	[self.split1 addChildDataSource:objectDS1];
+
+	self.split2 = [[DCTSplitDataSource alloc] initWithType:DCTSplitDataSourceTypeRow];
+	[self.split2 addChildDataSource:objectDS2];
+
+	DCTSplitDataSource *split = [[DCTSplitDataSource alloc] initWithType:DCTSplitDataSourceTypeSection];
+	[split addChildDataSource:self.split1];
+	[split addChildDataSource:self.split2];
+
+	self.dataSource = [[DCTCollectionViewDataSource alloc] initWithCollectionView:self.collectionView dataSource:split];
 	self.dataSource.cellReuseIdentifier = @"cell";
 }
 
-- (void)toggle {
+- (void)toggleList1 {
 
-	if ([self.split.childDataSources containsObject:self.list])
-		[self.split removeChildDataSource:self.list];
+	if ([self.split1.childDataSources containsObject:self.list1])
+		[self.split1 removeChildDataSource:self.list1];
 	else
-		[self.split addChildDataSource:self.list];
+		[self.split1 addChildDataSource:self.list1];
+}
+
+- (void)toggleList2 {
+
+	if ([self.split2.childDataSources containsObject:self.list2])
+		[self.split2 removeChildDataSource:self.list2];
+	else
+		[self.split2 addChildDataSource:self.list2];
 }
 
 #pragma mark - DCTCollectionViewDataSourceDelegate
@@ -57,8 +78,12 @@
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
 	id object = [self.dataSource objectAtIndexPath:indexPath];
 
-	if ([object isKindOfClass:[NSString class]] && [object isEqualToString:@"zero"])
-		[self toggle];
+	if (![object isKindOfClass:[NSString class]]) return;
+
+	if ([object isEqualToString:@"zero"])
+		[self toggleList1];
+	else if ([object isEqualToString:@"eleven"])
+		[self toggleList2];
 }
 
 @end
